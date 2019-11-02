07f576bb385de7f88663566e47a3dea106e98a2c
jgarciaruiz <github@snaven.com>
2019-11-02 22:09:26 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Users List</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <router-link to="/about"><ion-button expand="block" size="small">Go to About Page</ion-button></router-link>         
+      <ion-button @click="fetchUsers">Fetch users</ion-button>
+
+      <ion-list v-if="users && users.length">
+        <ion-item v-for="user in users" :key="user.id">
+          <ion-label>{{user.name}}</ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-content>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+ import axios from 'axios';
 
-export default {
+ export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      users: [],
+      errors: []
+    }
+  },
+  methods: {
+    fetchUsers: function () {
+      const baseURI = 'https://jsonplaceholder.typicode.com/users'
+      axios.get(baseURI)
+      .then((result) => {
+        this.users = result.data
+      })
+    }
   }
-}
+};
 </script>
